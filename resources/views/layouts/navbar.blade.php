f0e8f54af142c0e079e46ffb4222a972e9a0d2f1
devLateef <lateefolatayo@gmail.com>
2024-07-11 21:41:50 +0100

    Some changes made to my css

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -82,31 +82,6 @@
             </ul>
         </div>
     </div>
-    {{-- <div class="modal fade" id="approveModal" tabindex="-1"
-                aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Complete the form
-                                to Approve</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="row">
-                                <div class="form-group col-md-6 col-12">
-                                    <dt>Expected Start Date</dt>
-                                    
-                                </div>
-                                <div class="form-group col-md-6 col-12">
-                                    <dt>Expected End Date</dt>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-    </div> --}}
 </nav>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js"></script>
